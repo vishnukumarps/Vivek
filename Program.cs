58e04e56ef1ee2cfa72d0e1708d7b010e0c45bc3
vishnukumarps <vishnukumar29518@gmail.com>
2020-06-19 10:19:18 +0530

    test

--- Program.cs
+++ Program.cs
@@ -6,6 +6,17 @@ namespace Vivek
     {
         static void Main(string[] args)
         {
+            int a, b, Sum;
+            Console.WriteLine("Enter 2 numbers");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            Sum = a + b;
+            Console.WriteLine("Sum=" + Sum);
+            Console.WriteLine("Sum={0}", Sum);
+            Console.WriteLine("{0} + {1} = {2}", a, b, Sum);
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
             Console.WriteLine("Hello World!");
         }
     }
